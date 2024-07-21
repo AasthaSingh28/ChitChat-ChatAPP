0b5e2e7c9ecb42ce61ff3c229c8365407bc41c91
Aastha Singh <138100998+AasthaSingh28@users.noreply.github.com>
2024-07-21 22:36:41 +0530

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,9 +2,7 @@ import mongoose from 'mongoose';
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI, {
-      serverSelectionTimeoutMS: 5000,
-    });
+    await mongoose.connect(process.env.MONGO_DB_URI);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
