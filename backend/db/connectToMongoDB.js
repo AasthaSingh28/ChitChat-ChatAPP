ffe4e930cd89ced3a26d55a20855c561d18ed674
Aastha Singh <138100998+AasthaSingh28@users.noreply.github.com>
2024-07-21 22:19:13 +0530

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,19 +1,16 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const connectToMongoDB = async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_DB_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            connectTimeoutMS: 30000,
-        });
-        mongoose.connection;
-        console.log("connected to mongodb");
-    }
-    catch(error){
-        console.log("Error connecting to mongodb", error.message);
-    }
-
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    console.error('Full error:', error);
+  }
 };
 
 export default connectToMongoDB;
+
